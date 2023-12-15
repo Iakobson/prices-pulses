1e494ef2f9cc283ab60b33c84d754e3d8d886fb0
Sebastian <139753003+Iakobson@users.noreply.github.com>
2023-12-15 17:10:45 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+// @path: @/app/page.tsx
+import styles from './page.module.css'
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>  
+      <p>
+        Get started by editing&nbsp;
+        <code className={styles.code}>app/page.tsx</code>
+      </p>
+       
+      <Container>
+        <Typography variant="h2">Hello NEXT World</Typography>
+      </Container>
+
+
+        <p>Find in-depth information about Next.js features and API.</p>
+    </main>
+  )
+}
