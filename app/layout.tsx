e97e21f10ab496035548b7e08ee011ba9a3177d6
Sebastian <139753003+Iakobson@users.noreply.github.com>
2023-12-15 17:11:03 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Price-Pulse App',
+  description: 'Information about the lowest prices available in retail chains of Ukraine.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
