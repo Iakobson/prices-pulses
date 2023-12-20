cb61e033e5d964a8466bdf77b264c72a11a4f666
Sebastian <139753003+Iakobson@users.noreply.github.com>
2023-12-20 17:40:35 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,36 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+// @/app/layout.tsx
+import './globals.css';
+import ThemeRegistry from '@/views/ThemeRegistry/ThemeRegistry';
+// MUI components
+import Box from '@mui/material/Box';
+// views (design) elements
+import { DrawerHeader } from '@/views/Layout/DrawerHeader';
+import AppNavigation from '@/views/Layout/AppNavigation';
 
-const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Price-Pulse App',
   description: 'Information about the lowest prices available in retail chains of Ukraine.',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }:{ children:React.ReactNode }) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
+      <body>
+        <ThemeRegistry>
+          <Box sx={{ display: 'flex' }}>
+            {/* menu and site navigation */}
+            <AppNavigation />
+            {/* the main content of the site */}
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+              <DrawerHeader />
+              {children}
+            </Box>
+
+          </Box>
+        </ThemeRegistry>
       </body>
     </html>
-  )
+  );
 }
