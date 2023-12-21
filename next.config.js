a765d8b026d3c809977f8819b74a602e1fa8300d
Sebastian <139753003+Iakobson@users.noreply.github.com>
2023-12-21 16:58:04 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // environment variables for use in client and server side
+  env: {
+    DRAWER_WIDTH: '200',
+    API_URL: "http://localhost:3000",
+    DB_LOCAL_URI: "mongodb://127.0.0.1:27017/pricepulse",
+    DB_URI: "",
+  },
+  reactStrictMode: true,
+  // settings for optimized image loading
+  images: {
+    // trusted domains for image uploads
+    domains: ['source.unsplash.com', 'res.cloudinary.com', 'firebasestorage.googleapis.com', '192.168.1.3'],
+  } 
+}
 
 module.exports = nextConfig
