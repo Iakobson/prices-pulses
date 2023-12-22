377a2e83482d8e13bc14fd041d9e558e6d370d29
Sebastian <139753003+Iakobson@users.noreply.github.com>
2023-12-22 12:27:43 +0200

    Update BeverageCard.tsx

--- src/views/Categories/BeverageCard.tsx
+++ src/views/Categories/BeverageCard.tsx
@@ -1,11 +1,11 @@
 // @/views/Categories/BeverageCard.tsx
-import * as React from 'react';
 import Image from 'next/image';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
 
 import {IBeverage} from '@/models/interfaces';
 import BeverageCardTable from '@/views/Categories/BeverageCardTable';
@@ -17,16 +17,18 @@ type BeverageCardProps = {
 const BeverageCard:React.FC<BeverageCardProps> = ({ beverage }) => {
   const { prices } = beverage;
   return (
-    <Card sx={{ maxWidth: 400 }}>
-      <Image src="/images/beverages/jameson-07.webp"
-        alt="Random image"
+    <Card sx={{ maxWidth: { xs: "100%", sm: 400, md: 600, lg: 800 }, 
+	  boxShadow: 3, borderRadius: 8 }}
+	>
+      <Image src={beverage.imageUrl}
+        alt={beverage.imageUrl}
         width={320} height={320}
       />
       <CardContent>
-        <Typography variant="body1">
+        <Typography variant="body1" sx={{ color: red[500] }}>
           {beverage.category}
         </Typography>
-        <Typography gutterBottom variant="h3" component="div">
+        <Typography variant="h3" component="div" gutterBottom>
           {beverage.title}
         </Typography>
         <Typography variant="body2">
@@ -35,13 +37,13 @@ const BeverageCard:React.FC<BeverageCardProps> = ({ beverage }) => {
         {/* Ціни */}
         <Typography variant="h4">Ціни:</Typography>
         <BeverageCardTable prices={prices} />
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2">
           {beverage.description}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Редагувати</Button>
-        <Button size="small">Дивитися</Button>
+        <Button size="small" color="primary">Редагувати</Button>
+        <Button size="small" color="secondary">Дивитися</Button>
       </CardActions>
     </Card>
   );
