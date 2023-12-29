edbaf72ab22ee82bab0e6fe55fbb604b1144a18a
Sebastian <139753003+Iakobson@users.noreply.github.com>
2023-12-29 13:39:13 +0200

    Update SignInForm.tsx

--- src/views/SignInForm.tsx
+++ src/views/SignInForm.tsx
@@ -15,6 +15,8 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+import backPic from '../../public/images/backsingin.jpg'
+
 export default function SignInForm() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -28,13 +30,10 @@ export default function SignInForm() {
   return (
       <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
+        <Grid item 
+		  xs={false} sm={4} md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundImage: `url(${backPic.src})`,
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
@@ -45,11 +44,8 @@ export default function SignInForm() {
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              my: 8, mx: 4, display: 'flex',
+              flexDirection: 'column', alignItems: 'center',
             }}
           >
             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
@@ -59,34 +55,22 @@ export default function SignInForm() {
               Sign in
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
+              <TextField name="email"
+                margin="normal" required fullWidth
+                id="email" label="Email Address"
+                autoComplete="email" autoFocus
               />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
+              <TextField type="password" name="password"
+                margin="normal" required fullWidth
+                label="Password" id="password"
                 autoComplete="current-password"
               />
               <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Remember me"
               />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
+              <Button type="submit"
+                fullWidth variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
                 Sign In
@@ -109,6 +93,3 @@ export default function SignInForm() {
       </Grid>
   );
 }
-
-
-
